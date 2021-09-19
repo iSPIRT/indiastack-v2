b85f6774e2cf333dfabfba54b26630df081dcd62
Siddharth Ashok <siddharthashokg@gmail.com>
2021-09-19 14:48:48 +0530

    gitignore and build folder

---
+++ build/js/app.js
@@ -0,0 +1,2 @@
+"use strict";$(document).foundation(),$(document).ready((function(){$(".indiastack-is-slider").slick({dots:!1,arrows:!1,infinite:!0,speed:500,fade:!0,cssEase:"linear",autoplay:!0,autoplaySpeed:2e3}),$(".timeline-wrap").slick({dots:!1,arrows:!1,infinite:!1,slidesToShow:3,slidesToScroll:1}),$(".timeline-next").click((function(){$(".timeline-wrap").slick("slickNext")})),$(".timeline-previous").click((function(){$(".timeline-wrap").slick("slickPrev")}))}));
+//# sourceMappingURL=app.js.map
